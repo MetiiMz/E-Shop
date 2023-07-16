045b44dba426579ceeda095492cbac5e67a8cc3c
Mehdi Mz <mehdi.mousazadeh.n0@gmail.com>
2023-07-17 01:05:15 +0330

    create cart remove and context processors for cart length

--- orders/cart.py
+++ orders/cart.py
@@ -17,11 +17,14 @@ class Cart:
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
-            cart[str(product.id)]['product'] = product.name
+            cart[str(product.id)]['product'] = product
 
         for item in cart.values():
             item['total_price'] = int(item['price']) * item['quantity']
             yield item
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity):
         product_id = str(product.id)
@@ -29,6 +32,12 @@ class Cart:
             self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
         self.cart[product_id]['quantity'] += quantity
         self.save()
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def get_total_price(self):
         return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
